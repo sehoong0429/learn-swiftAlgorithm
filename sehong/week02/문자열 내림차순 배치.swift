5e6e5458975ba121edbea62909187427c90b177b
sehong <tjsgh0801@naver.com>
2022-08-26 00:06:00 +0900

    2주차 문제1 문자열 내림차순 배치하기

---
+++ sehong/week02/문자열 내림차순 배치.swift
@@ -0,0 +1,12 @@
+//
+//  문자열 내림차순 배치.swift
+//  sehong
+//
+//  Created by Seoyeon Hong on 2022/08/26.
+//
+
+import Foundation
+
+func solution(_ s: String) -> String{
+    return String(s.sorted(by: >)) //알파벳 내림차순 정렬해서 출력!
+}
