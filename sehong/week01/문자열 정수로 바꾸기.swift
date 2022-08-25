5e82b2fe1028cf322b5b7a69d036a86475df559d
sehong <tjsgh0801@naver.com>
2022-08-25 15:59:14 +0900

    문제 4 문자열 정수로 바꾸기

---
+++ sehong/week01/문자열 정수로 바꾸기.swift
@@ -0,0 +1,11 @@
+//
+//  문자열 정수로 바꾸기.swift
+//  sehong
+//
+//  Created by Seoyeon Hong on 2022/08/25.
+//
+
+import Foundation
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
