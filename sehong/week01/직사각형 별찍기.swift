b67062b8966f300d59de73d0e454c0985196f602
sehong <tjsgh0801@naver.com>
2022-08-25 23:42:54 +0900

    직사각형 별찍기 문제 주석 추가

--- sehong/week01/직사각형 별찍기.swift
+++ sehong/week01/직사각형 별찍기.swift
@@ -12,4 +12,5 @@ let (a, b) = (n[0], n[1])
 
 for _ in 0..<b{ //입력 받은 b까지 for문 돌리고
     print(Array(repeating: "*", count: a).joined()) // *을 입력받은 a만큼 반복해서 출력
+    //print(String(repeating: "*", count: a) 라고 해도 됨.
 }
