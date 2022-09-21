42ca67fc855ffe9b946383942831a673a6b3ab07
Jun048 <maxbet0408@gmail.com>
2022-09-21 11:27:38 +0900

    짝수와 홀수

---
+++ Max/Max/Week05/짝수와 홀수.swift
@@ -0,0 +1,14 @@
+//
+//  짝수와 홀수.swift
+//  Max
+//
+//  Created by JY on 2022/09/21.
+//
+
+func solution(_ num:Int) -> String {
+    if num % 2 == 0 {
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
