03d0469c2ed5a97c32c436fe855bb00375292e64
Jun048 <maxbet0408@gmail.com>
2022-10-05 12:30:13 +0900

    제일 작은 수 제거하기

---
+++ Max/Max/Week07/제일 작은 수 제거.swift
@@ -0,0 +1,26 @@
+//
+//  제일 작은 수 제거.swift
+//  Max
+//
+//  Created by JY on 2022/10/05.
+//
+
+import Foundation
+
+func solution(_ d:[Int], _ budget:Int) -> Int {
+    var result = 0
+    var sum = 0
+    let sorted = d.sorted()
+    for i in sorted{
+        if result + i <= budget{
+            sum += 1
+            result += i
+        }else{
+            break
+        }
+    }
+    return sum
+}
+
+//
+
