e08fa842707f34239dbeea9c0bfc83927315ba56
sehong <tjsgh0801@naver.com>
2022-10-29 10:21:59 +0900

    10주차 Add Two Integers

---
+++ sehong/week11/Add Two Integers.swift
@@ -0,0 +1,18 @@
+//
+//  Add Two Integers.swift
+//  sehong
+//
+//  Created by Seoyeon Hong on 2022/10/29.
+//
+
+import Foundation
+
+//Given two integers num1 and num2, return the sum of the two integers.
+
+class Solution {
+    func sum(_ num1: Int, _ num2: Int) -> Int {
+        
+        var answer = num1 + num2
+        return answer
+    }
+}
