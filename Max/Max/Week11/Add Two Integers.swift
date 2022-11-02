236f4f742c19e081471465691514a7724dcfab27
Jun048 <maxbet0408@gmail.com>
2022-11-02 14:22:26 +0900

    Add Two Integers

---
+++ Max/Max/Week11/Add Two Integers.swift
@@ -0,0 +1,15 @@
+//
+//  Add Two Integers.swift
+//  Max
+//
+//  Created by JY on 2022/11/02.
+//
+
+import Foundation
+
+class Solution {
+    func sum(_ num1: Int, _ num2: Int) -> Int {
+        var a = num1 + num2
+        return a
+    }
+}
