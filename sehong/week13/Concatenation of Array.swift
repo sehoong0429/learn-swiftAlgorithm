273ed300e852d017a113e9680749c295e76092c1
sehong <tjsgh0801@naver.com>
2022-11-16 13:48:28 +0900

    13주차 문제 Concatenation of Array

---
+++ sehong/week13/Concatenation of Array.swift
@@ -0,0 +1,14 @@
+//
+//  Concatenation of Array.swift
+//  sehong
+//
+//  Created by Seoyeon Hong on 2022/11/16.
+//
+
+import Foundation
+
+class Solution {
+    func getConcatenation(_ nums: [Int]) -> [Int] {
+        return nums+nums
+    }
+}
