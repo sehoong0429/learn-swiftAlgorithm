a4514b5caec7f052d604b04d02f3856069ab0d18
Jun048 <maxbet0408@gmail.com>
2022-11-23 16:46:53 +0900

    푸파대회

---
+++ Max/Max/Week14/푸파대회.swift
@@ -0,0 +1,19 @@
+//
+//  푸파대회.swift
+//  Max
+//
+//  Created by JY on 2022/11/23.
+//
+
+import Foundation
+
+func solution(_ food:[Int]) -> String {
+    var result = ""
+    
+    for i in (1..<food.count) {
+        result += String(repeating: "\(i)", count: food[i] / 2)
+    }
+
+    return result + "0" + String(result.reversed())
+}
+
