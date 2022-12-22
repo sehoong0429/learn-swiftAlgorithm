3aa06293cb7e0fafdb292bce2ea1bdd39af7338c
Hoonyyyy <hyunsoo34242686@gmail.com>
2022-12-22 13:04:31 +0900

    Flipping an Image

---
+++ Hoony/Hoony/Week 19/Flipping an Image.swift
@@ -0,0 +1,38 @@
+//
+//  Flipping an Image.swift
+//  Hoony
+//
+//  Created by 강현수 on 2022/12/22.
+//
+
+import Foundation
+
+// 처음에 문제 그냥 0이면 1나오게 1이면 0나오게 하라는건줄 알고 했다가
+// 그다음엔 이진행렬에 빠져서 그거 위키백과로 알아보다가... 생각해보니까 reversed() 키워드가
+// 간단하게 뒤집는다는건데
+// 가로로 바뀐다는거에 빠져서 완전 간단한 문제를 거진 1시간을 고민한거 같다
+// 덕분에 고등학교때나 했던 이진행렬을 다시 공부하는 계기가 됐다... 수능 행렬 간단한건 풀 수 있겠네...
+
+
+class Solution {
+    func flipAndInvertImage(_ image: [[Int]]) -> [[Int]] {
+        
+//        if image.count == 0 {
+//            return []
+//        }
+        
+        var reversedimage = image
+        
+        for i in 0..<reversedimage.count {
+            reversedimage[i] = reversedimage[i].reversed()
+            
+            for j in 0..<reversedimage.count {
+                var result = reversedimage[i][j] == 0 ? 1 : 0
+                reversedimage[i][j] = result
+            }
+        }
+        return reversedimage
+    }
+}
+
+// https://leetcode.com/problems/flipping-an-image/
